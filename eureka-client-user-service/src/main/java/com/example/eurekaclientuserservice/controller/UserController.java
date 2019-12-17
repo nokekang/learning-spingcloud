5fc2233c0106d59bfc94b30ef3bcd93a0fb2ba60
sunjk <sunjk@123.com>
2019-12-17 17:32:01 +0800

    第一章，eureka,user-service集群

--- eureka-client-user-service/src/main/java/com/example/eurekaclientuserservice/controller/UserController.java
+++ eureka-client-user-service/src/main/java/com/example/eurekaclientuserservice/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.example.eurekaclientuserservice.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("user")
 public class UserController {
 
+    @Value("${server.port}")
+    private String port;
+
     @GetMapping("hello")
     public String hello(){
-        return "hello:eureka-client-user";
+        return "hello:eureka-client-user" + "userPort1:" + port;
     }
 
 }
